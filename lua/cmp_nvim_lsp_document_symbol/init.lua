3166c3f22469fe9f6cfc0dded2c458d767b67c1c
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-05-28 09:51:08 +0200

    fix: avoid nil kinds_to_show table

--- lua/cmp_nvim_lsp_document_symbol/init.lua
+++ lua/cmp_nvim_lsp_document_symbol/init.lua
@@ -51,7 +51,11 @@ end
 
 source._get_kinds_to_show = function(self, params)
   local ft = vim.bo.filetype
-  return params.option.kinds_to_show[ft] or defaults.kinds_to_show
+  vim.print(params.option)
+  if params.option.kinds_to_show then
+    return params.option.kinds_to_show[ft] or defaults.kinds_to_show
+  end
+  return defaults.kinds_to_show
 end
 
 source.complete = function(self, params, callback)
